d4b1688df9f23768a9d44f9ab9570e72c8ab57e3
Leo Cookson <10001481@BCS.NET.NZ>
2017-08-28 10:55:32 +1200

    First

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Clear();
+			
+            Console.WriteLine("This program will display the number you type in");
+            var number = 0;
+
+            Console.WriteLine("Please enter a number");
+            var isNumber = int.TryParse(Console.ReadLine(), out number);
+
+            if(isNumber){
+                Console.WriteLine($"The number is {number}");
+            }
+            else{
+                Console.WriteLine("You did not type in a number");
+            }
+
+			
+            
+            
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+    }
+}
